040ad9b04f12052902060d0da1866a431ba50153
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-09-19 17:42:07 +1000

    added code for model/user.js to define schema using mongoose and giving it various fields, added virtual property to schema, created & imported user model

---
+++ models/User.js
@@ -0,0 +1,42 @@
+// Importing mongoose library from Node.js
+const mongoose = require('mongoose');
+
+// Defining user schema, using 'mongoose.Schema constructor with username, email, thoughts and friends fields
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: TextTrackCue,
+    match: [/.+@.+\..+/, 'Please enter a valid email address'],
+  },
+  thoughts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Thought',
+    },
+  ],
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+});
+
+// Adding virtual property to the schema, to compute length of 'friends' array
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+// Creating the User model using mongoose.model
+const User = mongoose.model('User', userSchema);
+
+// Exporting User model
+module.exports = User;
