022e7a87f6594341b5e9a6399c10408d0d77026f
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-09-20 20:54:15 +1000

    added thought-routes.js to handle routes for getting users,updating & deleting users & adding & deleting friend

---
+++ routes/api/user-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+// Import UserController from controllers folder
+const UserController = require('../controllers/UserController');
+
+// Handling the routes for getting all users and creating a new user
+router
+.route('/')
+.get(UserController.getAllUsers)
+.post(UserController.createUser)
+
+// Routes for getting, updating & deleting single user by ID
+router
+.route('/:userId')
+.get(UserController.getSingleUser)
+.put(UserController.updateUser)
+.delete(UserController.removeUser)
+
+// Routes for adding & deleting a friend from user's friend list
+router
+  .route('/:userId/friends/:friendId')
+  .post(UserController.addFriend)
+  .delete(UserController.removeFriend);
+
+module.exports = router;
